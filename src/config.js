d24ee4621c9fd605d6e224913bcdfafd7a93a738
Chirag Parakh <cparakh53@gmail.com>
2024-05-08 10:50:26 +0530

    final

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-// const levelsPasswords = [ 'owaspctf', 'heroEIN', 'XYqwert12', 'FXgfF2tQ', 'TideBreaker','rs7P0nyL', 'MOchX934CsD' , 'Fny4#HKh' , 'hundred' , 'GhPank34#lnPr' , 'MuPKinawnzz' , 'ARaR56Go' , 'vEzx89lop456qwu' , 'R3JheXNjYWxlIHdpdGggQWxwaGEK' , 'LndfeOfElsadfria' , 'MRRWM6LBOR3QU==='];
-const levelsPasswords = [ '1', '2', '3', '4', '5','6', '7' , '8' , '9' , '10' , '11' , '12' , '13' , '14' , '15' , '16'];
-
+const levelsPasswords = [ 'owaspctf', 'heroEIN', 'XYqwert12', 'FXgfF2tQ', 'TideBreaker','rs7P0nyL', 'MOchX934CsD' , 'Fny4#HKh' , 'hundred' , 'GhPank34#lnPr' , 'MuPKinawnzz' , 'ARaR56Go' , 'vEzx89lop456qwu' , 'R3JheXNjYWxlIHdpdGggQWxwaGEK' , 'LndfeOfElsadfria' , 'MRRWM6LBOR3QU==='];
 export default levelsPasswords;
